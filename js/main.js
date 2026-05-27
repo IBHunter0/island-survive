1db367ee9d54e93457ae7923e45b356aa677f0e1
IBHunter0 <349715219@gapps.yrdsb.ca>
2026-05-27 10:33:35 -0400

    Add main game initialization

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Initialize Phaser game
+const game = new Phaser.Game(GAME_CONFIG);
+
+// Game started
+console.log('Island Survival started!');
+console.log('Controls:');
+console.log('- Arrow keys or WASD: Move');
+console.log('- E: Interact');
+console.log('- C: Crafting menu');
+console.log('- I: Inventory');
